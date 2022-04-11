a70e078511fb14325735ac140c21076b36431e4c
SK SAHIL <sksahil.contact@gmail.com>
2022-04-11 23:18:06 +0530

    Largest Divisible Subset - leetcode 309

---
+++ dynamic-programming/longest-increasing-subsequence/largest-divisible-set.cpp
@@ -0,0 +1,45 @@
+/*
+    problem link: https://leetcode.com/problems/largest-divisible-subset/
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution
+{
+public:
+    vector<int> largestDivisibleSubset(vector<int> &nums)
+    {
+
+        sort(nums.begin(), nums.end());
+
+        vector<int> lis(nums.size(), 1), prev(nums.size(), -1);
+
+        int maxInd = 0;
+
+        for (int i = 0; i < nums.size(); i++)
+        {
+            for (int j = 0; j < i; j++)
+            {
+                if (nums[i] % nums[j] == 0 && lis[j] + 1 > lis[i])
+                {
+                    lis[i] = lis[j] + 1;
+                    prev[i] = j;
+                }
+            }
+            if (lis[maxInd] < lis[i])
+                maxInd = i;
+        }
+
+        vector<int> ans;
+        int j = maxInd;
+        while (j >= 0)
+        {
+
+            ans.push_back(nums[j]);
+            j = prev[j];
+        }
+
+        return ans;
+    }
+};
